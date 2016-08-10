58b28f5fbda0c9551c796152adb05660d8ed7e3e
William Tumeo <tumeowilliam@gmail.com>
2016-08-09 22:04:08 -0300

    Add file dislog + images

--- patchgui/main.py
+++ patchgui/main.py
@@ -1,6 +1,6 @@
 
 import sys, os.path
-# from PySide.QtCore import *
+from subprocess import call as process_call
 from PySide.QtGui import *
 
 if 'python.exe' in sys.executable:
@@ -13,26 +13,16 @@ if hasattr(sys, '_MEIPASS'):
 else:
     RES_PATH = PATH
 
-
-def get_ds_src():
-    pass
-
-def apply_patch():
-    pass
+PATCH_FILE = os.path.join(RES_PATH, 'phoenix.dt')
+XDELTA = os.path.join(RES_PATH, 'xdelta.exe')
 
 
-def get_patch_button():
-    buttonok = QPushButton("Aplicar")
-    buttonok.setEnabled(False)
-    buttonok.clicked.connect(apply_patch)
-    return buttonok
-
-
-class Form(QWidget):
+class Window(QWidget):
  
     def __init__(self, parent=None):
-        super(Form, self).__init__(parent)
+        super(Window, self).__init__(parent)
         self.setWindowTitle("Phoenix Wright | PT-BR")
+        self.setWindowIcon(QIcon(os.path.join(RES_PATH, 'icon.png')))
         self.fix_size(390, 355)
         
         app_group = QVBoxLayout()
@@ -41,28 +31,29 @@ class Form(QWidget):
         layout = QHBoxLayout()
         label = QLabel()
         label.setPixmap(QPixmap(os.path.join(RES_PATH, "please.jpg")))
-        # edit = QLineEdit()
-        # button = QPushButton("Selecionar...")
-        # layout.addWidget(edit)
-        # layout.addWidget(button)
         layout.addWidget(label)
         patchfile_g.setLayout(layout)
 
-        ds_g = QGroupBox("Arquivo DS")
+        ds_g = QGroupBox("Selecionar ROM")
         layout2 = QHBoxLayout()
-        edit2 = QLineEdit()
-        button2 = QPushButton("Selecionar...")
-        layout2.addWidget(edit2)
-        layout2.addWidget(button2)
+        self.edit = QLineEdit()
+        self.edit.textChanged.connect(self.check_file)
+        self.button_ds = QPushButton("Selecionar...")
+        self.button_ds.clicked.connect(self.get_ds_src)
+        layout2.addWidget(self.edit)
+        layout2.addWidget(self.button_ds)
         ds_g.setLayout(layout2)
 
         patch_g = QGroupBox()
         layout3 = QHBoxLayout()
-        self.cancel_button = QPushButton("Cancelar")
-        self.cancel_button.clicked.connect(sys.exit)
-        self.patch_button = get_patch_button()
+        self.quit_button = QPushButton("Fechar")
+        self.quit_button.clicked.connect(sys.exit)
         
-        layout3.addWidget(self.cancel_button)
+        self.patch_button = QPushButton("Aplicar")
+        self.patch_button.setEnabled(False)
+        self.patch_button.clicked.connect(self.apply_patch)
+        
+        layout3.addWidget(self.quit_button)
         layout3.addWidget(self.patch_button)
         patch_g.setLayout(layout3)
 
@@ -76,13 +67,26 @@ class Form(QWidget):
         self.resize(w, h)
         self.setMinimumSize(w, h)
         self.setMaximumSize(w, h)
-        
+    
+    def check_file(self):
+        if self.edit.text().endswith('.nds'):
+            self.patch_button.setEnabled(True)
+        else:
+            self.patch_button.setEnabled(False)
+    
+    def get_ds_src(self):
+        self.nds_file = QFileDialog.getOpenFileName(None, "Selecionar ROM", PATH, "Rom (*.*)")
+        self.edit.setText(self.nds_file[0])
+    
+    def apply_patch(self):
+        print(self.edit.text())
+        process_call([XDELTA, '-h'])
 
 
 def main():
     app = QApplication(sys.argv)
-    form = Form()
-    form.show()
+    window = Window()
+    window.show()
     return app.exec_()
 
 
